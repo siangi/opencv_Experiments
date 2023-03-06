9659de26a38b7909cca5e767a93101875ed47a86
siangi <siangi@gmx.ch>
2023-03-06 14:39:52 +0100

    histogramme

--- Basics/example1.js
+++ Basics/example1.js
@@ -1,40 +1,55 @@
-
 let imgElement = document.getElementById("imageSrc");
-imgElement.onload = showCannyEdges;
+imgElement.onload = equalizedImage;
 let inputElement = document.querySelector("#fileInput");
 
 inputElement.addEventListener(
-  "change",
-  (e) => {
-    imgElement.src = URL.createObjectURL(e.target.files[0]);
-  },
-  false
+    "change",
+    (e) => {
+        imgElement.src = URL.createObjectURL(e.target.files[0]);
+    },
+    false
 );
-
 
 function showImageOnCanvas() {
     let mat = cv.imread(imgElement);
     cv.imshow("outputCanvas", mat);
-    console.log('image width: ' + mat.cols + '\n' +
-            'image height: ' + mat.rows + '\n' +
-            'image size: ' + mat.size().width + '*' + mat.size().height + '\n' +
-            'image depth: ' + mat.depth() + '\n' +
-            'image channels ' + mat.channels() + '\n' +
-            'image type: ' + mat.type() + '\n');
+    console.log(
+        "image width: " +
+            mat.cols +
+            "\n" +
+            "image height: " +
+            mat.rows +
+            "\n" +
+            "image size: " +
+            mat.size().width +
+            "*" +
+            mat.size().height +
+            "\n" +
+            "image depth: " +
+            mat.depth() +
+            "\n" +
+            "image channels " +
+            mat.channels() +
+            "\n" +
+            "image type: " +
+            mat.type() +
+            "\n"
+    );
 
-    let row = 3, col = 4;
+    let row = 3,
+        col = 4;
     if (mat.isContinuous()) {
         let R = mat.ucharAt(row, col * mat.channels());
         let G = mat.ucharAt(row, col * mat.channels() + 1);
         let B = mat.ucharAt(row, col * mat.channels() + 2);
         let A = mat.ucharAt(row, col * mat.channels() + 3);
         let pixel = mat.ucharPtr(row, col);
-        console.log("color1 values of [3,4]", R,G,B,A, pixel);
+        console.log("color1 values of [3,4]", R, G, B, A, pixel);
     }
     mat.delete();
 }
 
-function showRegionOfInterest(){
+function showRegionOfInterest() {
     let full = cv.imread(imgElement);
     let region = new cv.Mat();
     let rect = new cv.Rect(150, 150, 100, 50);
@@ -45,21 +60,21 @@ function showRegionOfInterest(){
     region.delete();
 }
 
-function showGrayscale(){
+function showGrayscale() {
     let full = cv.imread(imgElement);
     let gray = new cv.Mat();
     cv.cvtColor(full, gray, cv.COLOR_RGBA2GRAY, 0);
-    
+
     cv.imshow("outputCanvas", gray);
     full.delete();
     gray.delete();
 }
 
-function showThresholdInRange(){
+function showThresholdInRange() {
     let full = cv.imread(imgElement);
     let thresholded = new cv.Mat();
-    let low = new cv.Mat(full.rows, full.cols, full.type(), [0,0,0,0]);
-    let high = new cv.Mat(full.rows, full.cols, full.type(), [150,150,150,255]);
+    let low = new cv.Mat(full.rows, full.cols, full.type(), [0, 0, 0, 0]);
+    let high = new cv.Mat(full.rows, full.cols, full.type(), [150, 150, 150, 255]);
     cv.inRange(full, low, high, thresholded);
 
     cv.imshow("outputCanvas", thresholded);
@@ -69,18 +84,18 @@ function showThresholdInRange(){
     high.delete();
 }
 
-function showDownscaled(){
+function showDownscaled() {
     let full = cv.imread(imgElement);
     let downscaled = new cv.Mat();
-    let newSize = new cv.Size(0, 0)
-    cv.resize(full, downscaled, newSize, 0.05, 0.05, cv.INTER_AREA)
+    let newSize = new cv.Size(0, 0);
+    cv.resize(full, downscaled, newSize, 0.05, 0.05, cv.INTER_AREA);
 
     cv.imshow("outputCanvas", downscaled);
     full.delete();
     downscaled.delete();
 }
 
-function showGaussianBlurred(){
+function showGaussianBlurred() {
     let full = cv.imread(imgElement);
     let blurred = new cv.Mat();
     let kSize = new cv.Size(5, 5);
@@ -90,7 +105,7 @@ function showGaussianBlurred(){
     blurred.delete();
 }
 
-function showCannyEdges(){
+function showCannyEdges() {
     let full = cv.imread(imgElement);
     let gray = new cv.Mat();
     let blurred = new cv.Mat();
@@ -98,11 +113,34 @@ function showCannyEdges(){
     let kSize = new cv.Size(5, 5);
     cv.cvtColor(full, gray, cv.COLOR_RGBA2GRAY, 0);
     cv.GaussianBlur(gray, blurred, kSize, 0, 0, cv.BORDER_DEFAULT);
-    cv.Canny(blurred, edges, 50, 100, 3, false)
+    cv.Canny(blurred, blurred, 50, 100, 3, false);
 
-    cv.imshow("outputCanvas", edges);
+    cv.imshow("outputCanvas", blurred);
     full.delete();
     gray.delete();
-    blurred.delete(); 
+    blurred.delete();
     edges.delete();
+}
+
+function calcHistogram() {
+    let base = cv.imread(imgElement);
+    cv.cvtColor(base, base, cv.COLOR_RGBA2GRAY, 0);
+    let srcVec = new cv.MatVector();
+    srcVec.push_back(base);
+    let histogram = new cv.Mat();
+    let mask = new cv.Mat();
+
+    cv.calcHist(srcVec, [0], mask, histogram, [256], [0, 255], false);
+    console.log(histogram.data);
+    console.log(histogram.data[250]);
+    cv.imshow("outputCanvas", base);
+    base.delete();
+}
+
+function equalizedImage() {
+    let base = cv.imread(imgElement);
+    cv.cvtColor(base, base, cv.COLOR_RGBA2GRAY, 0);
+    cv.equalizeHist(base, base);
+    cv.imshow("outputCanvas", base);
+    base.delete();
 }
