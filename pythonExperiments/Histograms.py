8f0e293c1042872936b886555001df348f61adf7
siangi <siangi@gmx.ch>
2023-03-20 10:26:39 +0100

    more histogram tries, cant extract which color it is

--- pythonExperiments/Histograms.py
+++ pythonExperiments/Histograms.py
@@ -1,17 +1,41 @@
 import cv2 as cv
 from matplotlib import pyplot
+import numpy as np
 
 def grayScaleHistogram(img):
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     hist = cv.calcHist([gray], [0], None,[256], [0,256])
     return hist
 
+def fullRGBHistogram(img):
+    rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    histDimensions = [256, 256, 256]
+    histRanges =[0, 256, 0,256, 0,256]
+    combined = cv.calcHist(rgb, [0,1,2], None, histDimensions, histRanges)
+    return combined
+
+def RGBMergedSinglesHistogram(img):
+    # Split the image into its color channels
+    b, g, r = cv.split(img)
+
+    # Set the histogram parameters
+    histSize = [256]
+    range = [0, 256]
+
+    # Calculate the histograms for each color channel
+    histR = cv.calcHist([r], [0], None, histSize, range)
+    histG = cv.calcHist([g], [0], None, histSize, range)
+    histB = cv.calcHist([b], [0], None, histSize, range)
+
+    hist = cv.merge([histR, histG, histB])
+    return hist
+
 def fullHSVHistogram(img):
-    hsv = cv.cvtColor(img, cv.COLOR_RGB2HSV)
-    histDimensions = [18,25,25]
+    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    histDimensions = [180,100,100]
     histRanges = [0,180,0,256,0,256]
-    histogram = cv.calcHist(hsv, [0,1,2], None, histDimensions, histRanges)
-    return histogram
+    combined = cv.calcHist(hsv, [0,1,2], None, histDimensions, histRanges)
+    return combined
 
 def biggestValuesLocations(histogram, amount):
     reshaped = histogram.copy()
@@ -25,10 +49,38 @@ def plotHistograms(histograms, colors):
     
     pyplot.show()
 
+def findMaxInMultidimensional(multiDim):
+    flattened = cv.merge(multiDim)
+    maxIdx = np.unravel_index(np.argmax(flattened), flattened.shape)
+    max = np.max(flattened)
+    return(max, maxIdx)
 
-img = cv.imread("D:\Studium\Bachelor\ArtVeeData\_Ammunition__ And remember _ bonds buy bullets_ _1918_ .jpg");
+def multipleLargestMultiDimensional(multiDim, amount, needsMerging):
+    # if needsMerging:
+    #     flattened = cv.merge([multiDim[0], multiDim[1], multiDim[2]])
+    # else:
+    flattened = multiDim
+
+    results = []
+    for i in range(0, amount):
+        flatIdx = np.argmax(flattened)
+        multiIdx = np.unravel_index(flatIdx, flattened.shape)
+        max = np.max(flattened)
+        flattened[multiIdx[0]][multiIdx[1]][multiIdx[2]] = -1
+        results.append((max, multiIdx))
+
+    return results
+
+
+
+img = cv.imread("C:\\Studium\\BPROJ\\opencv_Experiments\\pythonExperiments\\HistogramTestSwatch.png")
 # histogram = grayScaleHistogram(img)
 # plotHistograms([histogram], ["b"])
 
 hsvhist = fullHSVHistogram(img)
-biggestValuesLocations(hsvhist, 5)
+rgbHist = fullRGBHistogram(img)
+premerged = RGBMergedSinglesHistogram(img)
+print(multipleLargestMultiDimensional(premerged, 1, False))
+print(multipleLargestMultiDimensional(rgbHist, 1, True))
+
+# biggestValuesLocations(hsvhist, 5)
