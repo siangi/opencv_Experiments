8b830ae8306227268816af7c9f03a783f20464e8
siangi <siangi@gmx.ch>
2023-03-27 19:54:09 +0200

    created the frontend playground

---
+++ MasonryExperiments/masonry-test/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Gallery from './Components/Gallery';
+
+function App() {
+  return (
+    <div className="App">
+      <Gallery></Gallery>
+    </div>
+  );
+}
+
+export default App;
